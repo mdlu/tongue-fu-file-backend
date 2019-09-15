f36ee248d09d6afda3bbcfbba34d6da2ffa2531e
Mindren Lu <mdlu841@gmail.com>
2019-09-15 09:43:58 -0400

    combined routes in app

--- app.py
+++ app.py
@@ -23,21 +23,29 @@ def home():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename) # path to audio file
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) # saves locally
 
-            a = get_transcript(path) # retrieves the file again using the path
-            return json.dumps(a)
+            ts = get_transcript(path) # retrieves the file again using the path
+            colon = ts.index(':')
+            stripped_ts = ts[colon+4:]
 
-
-@app.route('/score', methods=['GET', 'POST'])
-@cross_origin()
-def score():
-    if request.method == 'GET':
-        return 
-    else:
-        if request.method == 'POST':
             orig = request.form["orig"]
-            sample = request.form["sample"]
-            a = int(twister_score.compare(orig, sample) * twister_score.score(orig) * 10) # multiplies accuracy by the difficulty of the tongue twister, and scales by 10
-            return json.dumps(a)
+            orig_score = twister_score.score(orig)
+
+            compare = twister_score.compare(orig, stripped_ts)
+            # a = int(twister_score.compare(orig, sample) * twister_score.score(orig) * 10) # multiplies accuracy by the difficulty of the tongue twister, and scales by 10
+            return json.dumps({"ts": ts, "stripped_ts": stripped_ts, "compare": compare, "orig_score": orig_score})
+
+
+# @app.route('/score', methods=['GET', 'POST'])
+# @cross_origin()
+# def score():
+#     if request.method == 'GET':
+#         return 
+#     else:
+#         if request.method == 'POST':
+#             orig = request.form["orig"]
+#             sample = request.form["sample"]
+#             a = int(twister_score.compare(orig, sample) * twister_score.score(orig) * 10) # multiplies accuracy by the difficulty of the tongue twister, and scales by 10
+#             return json.dumps(a)
 
 if __name__ == '__main__':
     app.run(debug=True)
